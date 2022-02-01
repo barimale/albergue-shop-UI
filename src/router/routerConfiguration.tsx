49cabdd40ea1b5c236e6e4d6808d9df61b86e25f
barimale <mateusz.wolnica@gmail.com>
2022-02-01 01:25:33 +0100

    .

---
+++ src/router/routerConfiguration.tsx
@@ -0,0 +1,19 @@
+
+export const appBaseRouteKey = "";
+
+export enum configSectionType {
+    'divider',
+    'link'
+}
+
+export type configSection = {
+    title: string;
+    api: string;
+    type: configSectionType;
+    id: string;
+}
+
+export function GetFullPathTo(items: configSection[], title: string): string{
+    const result = items.findIndex((p: configSection) => p.title === title);
+    return items[result].api;
+}
